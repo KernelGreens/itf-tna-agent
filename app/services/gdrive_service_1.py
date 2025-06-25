b0db17e760064eee99ee3de759dc08affefadf0c
Abiodun Muh'd-Ahmad Odukaye <abiodun.odukaye@mykinetic.app>
2025-06-25 17:14:52 +0100

    edited the google sheet name in the google drive service

--- app/services/gdrive_service_1.py
+++ app/services/gdrive_service_1.py
@@ -16,8 +16,8 @@ def fetch_sheet_data(sheet_url: str):
     sh = gc.open_by_url(sheet_url)
 
     # Assume tabs are named "HR", "Supervisors", "Staff"
-    df_hr = pd.DataFrame(sh.worksheet("HR").get_all_records())
-    df_sup = pd.DataFrame(sh.worksheet("Supervisors").get_all_records())
-    df_staff = pd.DataFrame(sh.worksheet("Staff").get_all_records())
+    df_hr = pd.DataFrame(sh.worksheet("ITF_TNA_HR").get_all_records())
+    df_sup = pd.DataFrame(sh.worksheet("ITF_TNA_Supervisors").get_all_records())
+    df_staff = pd.DataFrame(sh.worksheet("ITF_TNA_Operatives").get_all_records())
 
     return df_hr, df_sup, df_staff
